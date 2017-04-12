3b61247227eb7e41ee9aadfc726abf5a3f7086c5
todomato <dreamrrrr@hotmail.com>
2017-04-12 17:56:18 +0800

    實做 test1

---
+++ UnitTestProject1/ClassLibrary1/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Kata
+    {
+        public static string SpinWords(string str)
+        {
+            var ary = str.ToCharArray();
+            var newAry = ary.Reverse();
+            return new string(newAry.ToArray());
+
+        }
+    }
+}
