cb9cd76094dca007c0202013ee379e294e674525
todomato <dreamrrrr@hotmail.com>
2017-04-12 18:03:29 +0800

    實做test2

--- UnitTestProject1/ClassLibrary1/Kata.cs
+++ UnitTestProject1/ClassLibrary1/Kata.cs
@@ -9,9 +9,26 @@ namespace ClassLibrary1
     {
         public static string SpinWords(string str)
         {
-            var ary = str.ToCharArray();
-            var newAry = ary.Reverse();
-            return new string(newAry.ToArray());
+            var stringArray = str.Split(' ');
+            var newStrings = new List<string>();
+
+            for (int i = 0; i < stringArray.Count(); i++)
+            {
+                var ary = stringArray[i].ToCharArray();
+                if (ary.Count() >= 5)
+                {
+                    var newAry = ary.Reverse();
+                    newStrings.Add(new string(newAry.ToArray()));
+                }
+                else
+                {
+                    newStrings.Add(stringArray[i]);
+                }
+            }
+
+            var result = string.Join(" ", newStrings);
+
+            return result;
 
         }
     }
