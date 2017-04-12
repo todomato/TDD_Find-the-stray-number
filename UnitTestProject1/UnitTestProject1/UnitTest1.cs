6374d2d594057795967bd8fe36c4e5f3b3818b7b
todomato <dreamrrrr@hotmail.com>
2017-04-12 17:21:27 +0800

    init

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestFixture()]
+    public class CalculatorTests
+    {
+        [Test()]
+        public void SimpleArray1()
+        {
+            var array = new int[] { 1, 1, 2 };
+
+            var target = new Calculator();
+            var actual = target.Stray(array);
+
+            Assert.AreEqual(2, actual);
+        }
+
+        [Test()]
+        public void SimpleArray2()
+        {
+            var array = new int[] { 17, 17, 3, 17, 17, 17, 17 };
+
+            var target = new Calculator();
+            var actual = target.Stray(array);
+
+            Assert.AreEqual(3, actual);
+        }
+      
+    }
+}
